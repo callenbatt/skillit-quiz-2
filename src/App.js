eb46fa19ad357cda7b4c501c17c46d5314e6bb3a
callenbatt@gmail.com <callenbatt@gmail.com>
2019-09-29 11:25:42 -0400

    configuring routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import firebase, { FirebaseContext } from '../firebase';
+
+// import vg_inactive from './views/giver/inactive';
+// import vg_idle from './views/giver/idle';
+// import vg_ready from './views/giver/ready';
+// import vg_active from './views/giver/active';
+// import vg_analysis from './views/giver/analysis';
+// import vg_complete from './views/giver/complete';
+
+// import vt_inactive from './views/taker/inactive';
+// import vt_idle from './views/taker/idle';
+// import vt_ready from './views/taker/ready';
+// import vt_active from './views/taker/active';
+// import vt_analysis from './views/taker/analysis';
+// import vt_complete from './views/taker/complete';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <FirebaseContext.Provider value={{ user, firebase}}>
+                <Switch>
+                </Switch>
+            </FirebaseContext.Provider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
